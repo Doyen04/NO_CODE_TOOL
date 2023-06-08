3383c9214c69bde098fd6a4c11eddf0b68641f64
Doyen04 <solaopeyemi93@gmail.com>
2023-06-08 07:44:56 +0100

    ADDED MAIN INTERFACE

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>{logo}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+        <div className='App-container'>
+            <Header>
+                <MenuItem prop={'red'}/>
+                <MenuItem prop={'rew'}/>
+            </Header>
+            <Editor/>
+            <Header/>
+        </div> 
+    )
 }
 
+function Header({children}) {
+    return(  
+        <div className='header'>{children}</div>
+    )
+}
+function MenuItem({prop}) {
+    return (
+        <div className='menu-items'>{prop}</div>
+    )
+}
+function Editor() {
+    return(
+        <div className='editor-container'>
+            <SideBar/>
+            <PropertyBar/>
+            <Canvas/>
+            <PropertyBar/>
+            <SideBar/>
+        </div>
+    )
+}
+function SideBar({children}) {
+    return(
+        <div className='side-bar'></div>
+    )
+}
+function PropertyBar( ) {
+    return(
+        <div className='property-bar'></div>
+    )
+}
+function Canvas( ) {
+    return(
+        <div className='canvas-container'>ffff </div>
+    )
+}
 export default App;
 
 //how do react prevent js from show error for none js syntax
+//how to pass parameters to component
+//why must the argument for component be children
